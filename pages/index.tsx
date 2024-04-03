c3af20e5e43e3d28119659cc7da229190a40a890
Tomoyo Alvåg <tomoyo.tomoyo.m@gmail.com>
2024-04-03 16:28:33 +0200

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,47 @@
-import Link from "next/link";
-import Layout from "../components/Layout";
+import { useState } from "react";
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
-);
+const ToDoList = () => {
+  // Use meaningful and pronounceable variable names
+  //Use todo,setTodo/ newTodo, setNewTodo
+  const [t, sT] = useState([]);
+  const [iV, sIV] = useState("");
 
-export default IndexPage;
+  // Use explanatory variables
+  //
+  const addTodo = () => {
+    //Avoid Mental Mapping
+    const isInputValueValid = iV.trim() !== "";
+    if (isInputValueValid) {
+      // Use the same vocabulary for the same type of variable
+      sT([...t, iV]);
+      sIV("");
+    }
+  };
+
+  // Functions should do one thing
+  // 関数は一つのことを行う
+  const removeTodo = (index: number) => {
+    // Rule: Use default parameters instead of short circuiting or conditionals
+    // 短絡評価や条件文の代わりにデフォルトパラメータを使用する
+    const updatedTodoList = t.filter((_, ind) => ind !== index);
+    sT(updatedTodoList);
+  };
+
+  return (
+    <div>
+      <h1>ToDo List</h1>
+      <input type="text" value={iV} onChange={(e) => sIV(e.target.value)} />
+      <button onClick={addTodo}>Add</button>
+      <ul>
+        {t.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => removeTodo(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ToDoList;
