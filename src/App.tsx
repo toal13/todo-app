c5f8d23e94495d907e3b32674d5f2d9ac542b2f7
Tomoyo Alvåg <tomoyo.tomoyo.m@gmail.com>
2024-04-04 14:01:11 +0200

    add files

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,36 @@
-import React, { useState } from 'react';
+import { useState } from "react";
 
 const ToDoList = () => {
   // Rule: Use meaningful and pronounceable variable names
-  // 意味のある変数名を使用
+  // ex. todo, setTodo
   const [t, sT] = useState([]);
-  const [iV, sIV] = useState('');
+  const [iV, sIV] = useState("");
 
-  // Rule: Use explanatory variables
-  // 説明的な変数を使用
+  // Use explanatory variables
   const addTodo = () => {
-    // Rule: Avoid Mental Mapping
-    // メンタルマッピングの回避
-    const isInputValueValid = iV.trim() !== '';
+    //Avoid Mental Mapping
+    //
+    const isInputValueValid = iV.trim() !== "";
     if (isInputValueValid) {
-      // Rule: Use the same vocabulary for the same type of variable
-      // 同じタイプの変数には同じ語彙を使用する
+      // Use the same vocabulary for the same type of variable
       sT([...t, iV]);
-      sIV('');
+      sIV("");
     }
   };
 
   // Rule: Functions should do one thing
-  // 関数は一つのことを行う
+  //
   const removeTodo = (index: number) => {
-    // Rule: Use default parameters instead of short circuiting or conditionals
-    // 短絡評価や条件文の代わりにデフォルトパラメータを使用する
+    // Use default parameters instead of short circuiting or conditionals
+    //
     const updatedTodoList = t.filter((_, ind) => ind !== index);
     sT(updatedTodoList);
   };
 
   return (
-    <div>
+    <div className="">
       <h1>ToDo List</h1>
-      <input
-        type="text"
-        value={iV}
-        onChange={(e) => sIV(e.target.value)}
-      />
+      <input type="text" value={iV} onChange={(e) => sIV(e.target.value)} />
       <button onClick={addTodo}>Add</button>
       <ul>
         {t.map((todo, index) => (
